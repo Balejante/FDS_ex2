a143b061a9f49a85e5f94fb34149703ba3ae7e19
VitorCaus <vitor50033@outlook.com>
2023-08-22 14:42:54 -0300

    versao completa

--- FDS_ex2/src/App.java
+++ FDS_ex2/src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args){
-        
+        Conta teste = new Conta.Builder(1, "Joao")
+                                .Build();
     }
     
 }
--- FDS_ex2/src/Conta.java
+++ FDS_ex2/src/Conta.java
@@ -1,6 +1,6 @@
 public class Conta{
     enum Categoria{NORMAL, ADVANCED, PREMIUM};
-    Long numero;
+    long numero;
     String nomeCorrentista;
     int saldoLivreInicial;
     int salAplicacaoInicial;
@@ -8,13 +8,35 @@ public class Conta{
     double taxaSaldoNegativo;
     Categoria categoriaInicial;
 
-    public Conta(Long numero, String nomeCorrentista, int saldoLivreInicial, int salAplicacaoInicial, double taxaRemuneracao, double taxaSaldoNegativo, Categoria categoriaInicial){
-        this.numero = numero;
-        this.nomeCorrentista = nomeCorrentista;
-        this.saldoLivreInicial = saldoLivreInicial;
-        this.salAplicacaoInicial = salAplicacaoInicial;
-        this.taxaRemuneracao = taxaRemuneracao;
-        this. taxaSaldoNegativo = taxaSaldoNegativo;
-        this.categoriaInicial = categoriaInicial;
+    public static class Builder{
+        long numero;
+        String nomeCorrentista;
+        int saldoLivreInicial = 0;
+        int salAplicacaoInicial = 0;
+        double taxaRemuneracao = 0.0;
+        double taxaSaldoNegativo = 0.0;
+        Categoria categoriaInicial = Categoria.NORMAL;
+
+        public Builder(long numero, String nomeCorrentista){
+            this.numero = numero;
+            this.nomeCorrentista = nomeCorrentista;
+        }
+
+        public Builder saldoLivreInicial(int saldoLivreInicial){this.saldoLivreInicial = saldoLivreInicial; return this;}
+        public Builder saldoAplicacaoInicial(int salAplicacaoInicial){this.salAplicacaoInicial = salAplicacaoInicial; return this;}
+        public Builder taxaRemuneracao(double taxaRemuneracao){this.taxaRemuneracao = taxaRemuneracao; return this;}
+        public Builder taxaSaldoNegativo(double taxaSaldoNegativo){this.taxaSaldoNegativo = taxaSaldoNegativo; return this;}
+        public Builder categoriaInicial(Conta.Categoria catInicial){this.categoriaInicial = catInicial; return this;}
+        public Conta Build(){return new Conta(this);}
+    }
+
+    private Conta(Builder builder){
+        this.numero = builder.numero;
+        this.nomeCorrentista = builder.nomeCorrentista;
+        this.saldoLivreInicial = builder.saldoLivreInicial;
+        this.salAplicacaoInicial = builder.salAplicacaoInicial;
+        this.taxaRemuneracao = builder.taxaRemuneracao;
+        this.taxaSaldoNegativo = builder.taxaSaldoNegativo;
+        this.categoriaInicial = builder.categoriaInicial;
     }
 }
